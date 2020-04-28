dc7a7645a15a3b2b9a4de5ae2ddcf1ddc9debc9b
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-04-28 16:57:12 -0500

    create schema

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const cTable = require("console.table");
+
+//set up connection to MySQL
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Nashvegas22",
+    database: "employee_db"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    
+    console.log("Successfully connected as id " + connection.threadId);
+    // afterConnect();
+  });
