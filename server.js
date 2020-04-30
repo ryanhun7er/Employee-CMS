2178e0470a28f40cb4f97b1000d6962a54fe3cc5
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-04-30 18:18:44 -0500

    update employee closes server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cTable = require("console.table");
 require('dotenv').config();
 
 //set up connection to MySQL
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: process.env.HOST,
     port: 3306,
     user: process.env.USER,
@@ -37,22 +37,85 @@ var connection = mysql.createConnection({
             "Update Employee",
             "Exit"
           ]
-      })
-
-      .then(response => {
+      }).then(response => {
           const selection = response.choice1;
           switch(selection) {
+            
+            //add department
+            case "Add Department":
+                connection.query("SELECT * FROM department", function(err, response) {
+                    if(err) throw err;
+                    inquirer.prompt([
+                        {
+                            type: "input",
+                            name: "dept_name",
+                            message: "Which department would you like to add?"
+                        },
+                    ]).then(answer => {
+                        connection.query("INSERT INTO department SET ?",
+                        {
+                            name: answer.dept_name
+                        },
+                        connection.query("SELECT * FROM department", function(err, response) {
+                            if(err) throw err;
+                            console.table(response);
+                            employeeQ();
+                        }))
+                    })
+                });
+                break;
+            
+            //add roles
+            case "Add Role":
+                connection.query("SELECT * FROM department", function(err, response) {
+                    if(err) throw err;
+                    inquirer.prompt([
+                        {
+                            name: "title",
+                            type: "input",
+                            message: "What role would you like to add?",
+
+                        },
+
+                        {
+                            name: "salary",
+                            type: "input",
+                            message: "Input salary for this role"
+                        },
+
+                        {
+                            name: "dept_id",
+                            type: "input",
+                            message: "Select the department for this role",
+                            choices: () => response.map(department => `${department.id} ${department.name}`)
+
+                        }
+                    ]).then(answer => {
+                        connection.query("INSERT INTO roles SET ?",
+                        {
+                            title: answer.title,
+                            salary: answer.salary,
+                            dept_id: answer.department_id
+                        },
+                        function(err) {
+                            if(err) throw err;
+                        
+                        console.log(`${answer.title} added to database`);
+                        connection.query("SELECT * FROM roles", function(err, response) {
+                            if(err) throw err;
+                            console.table(response);
+                            employeeQ();
+                        })
+                    })
+                })
+            });
+            break;
 
             //add employee
             case "Add Employee":
-                connection.query(
+                connection.query("SELECT * FROM roles", function(err, response) {
+                    if(err) throw err;               }
                     inquirer.prompt([
-                    {
-                        type: "input",
-                        name: "firstName",
-                        message: "What is the employee's first name?"
-                    },
-
                     {
                         type: "input",
                         name: "first_name",
@@ -62,14 +125,14 @@ var connection = mysql.createConnection({
                     {
                         type: "input",
                         name: "last_name",
-                        message: "What is the employee's first name?"
+                        message: "What is the employee's last name?"
                     },                
                     
                     {
                         type: "list",
                         name: "role_id",
                         message: "What is the employee's role?",
-                        choices: () => roles.map(roles => `${roles.id} ${roles.title}`)
+                        choices: () => response.map(roles => `${roles.id} ${roles.title}`)
                     },
 
                     {
@@ -78,6 +141,7 @@ var connection = mysql.createConnection({
                         message: "Who is the employee's Manager?",
                         choices: () => employee.map(employee => `${employee.id} ${employee.name}`)
                     }
+                
                 ])
                 .then(answer => {
                     connection.query("INSERT INTO employee SET ?",
@@ -95,11 +159,13 @@ var connection = mysql.createConnection({
                             if (err) throw err;
                             console.table(response);
                             employeeQ();
+                        
                         })
                     })
                 })
             )
-                break;
+            ;
+            break;
 
             case "View Departments":
                 connection.query("SELECT * FROM department", function(err, response) {
@@ -125,7 +191,11 @@ var connection = mysql.createConnection({
                 });
                 break;     
           
-          
+            case "Finish":
+                connection.end();
+                    break;
+                    default: 
+                    break;
           
           
           
